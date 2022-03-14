0cf778d78306d984e0663710a7f1f34794af8627
Domincianoo <101437074+Domincianoo@users.noreply.github.com>
2022-03-13 22:13:42 -0300

    Add files via upload

---
+++ challenge/api/api/Controllers/TakeblipReposirotyController.cs
@@ -0,0 +1,47 @@
+﻿using api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [ApiController]
+    public class TakeblipReposirotyController : ControllerBase
+    {
+
+        private readonly ILogger<TakeblipReposirotyController> _logger;
+
+        public TakeblipReposirotyController(ILogger<TakeblipReposirotyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public async Task<string> Get()
+        {
+            List<TakeblipRepository> takeblipRepositories = new List<TakeblipRepository>();
+
+            //Utilizando o nuget Octokit, simplificaremos o acesso à API do Github, não sendo necessário: informar a URL da API, o método(Get, Post, Patch, etc...), alterar o cabeçalho do protocolo HTTP(para informar o modo de autenticação,por exemplo), criar as classes para receber o Json retornado da API.
+            var github = new GitHubClient(new ProductHeaderValue("ChatbotAPI"));
+
+            github.Credentials = new Credentials("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");
+
+            var allRepositories = await github.Repository.GetAllForUser("takenet");
+
+            var csharpRepositories = allRepositories.Where(a => a.Language == "C#").OrderBy(x => x.CreatedAt).ToList();
+            var olderCSharpRepositories = csharpRepositories.Take(5).ToList();
+
+            olderCSharpRepositories.ForEach(x => takeblipRepositories.Add(new TakeblipRepository() { FullName = x.FullName, Description = x.Description }));
+
+            string output = JsonConvert.SerializeObject(takeblipRepositories);
+
+            return output;
+        }
+    }
+}
---
+++ challenge/api/api/Models/TakeblipRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class TakeblipRepository
+    {
+        public string FullName { get; set; }
+        public string Description { get; set; }
+    }
+}
